49e42183f9beec298808d2c228d983e0803dd04e
Luisinho01 <91190561+Luisinhi010@users.noreply.github.com>
2026-08-24 14:34:21 -0300

    feat: add profile navigation and copy ID actions (ChatGPT)

--- Unicord.Universal/Dialogs/ProfileOverlay.xaml.cs
+++ Unicord.Universal/Dialogs/ProfileOverlay.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using Unicord.Universal.Models.User;
 using Unicord.Universal.Services;
+using Windows.ApplicationModel.DataTransfer;
 using Windows.System;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
@@ -27,6 +28,41 @@ namespace Unicord.Universal.Dialogs
         public ProfileOverlay()
         {
             InitializeComponent();
+            Loaded += ProfileOverlay_Loaded;
+        }
+
+        private void ProfileOverlay_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (NavView != null && OverviewItem != null)
+                NavView.SelectedItem = OverviewItem;
+
+            ShowSection("overview");
+        }
+
+        private void NavView_SelectionChanged(Microsoft.UI.Xaml.Controls.NavigationView sender, Microsoft.UI.Xaml.Controls.NavigationViewSelectionChangedEventArgs args)
+        {
+            var tag = args.SelectedItemContainer?.Tag as string;
+            ShowSection(tag ?? "overview");
+        }
+
+        private void ShowSection(string section)
+        {
+            if (OverviewContent == null || MutualServersContent == null)
+                return;
+
+            var showMutual = section == "mutual";
+            OverviewContent.Visibility = showMutual ? Visibility.Collapsed : Visibility.Visible;
+            MutualServersContent.Visibility = showMutual ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        private void CopyUserId_Click(object sender, RoutedEventArgs e)
+        {
+            if (User == null)
+                return;
+
+            var package = new DataPackage();
+            package.SetText(User.Id.ToString());
+            Clipboard.SetContent(package);
         }
 
         private void DropShadowPanel_PreviewKeyUp(object sender, KeyRoutedEventArgs e)
